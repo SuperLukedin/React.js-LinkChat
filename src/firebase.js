24c80ba7020a45cf3a6d933d66e0705eab09175a
Yuchen Peng <yuchenpeng100@gmail.com>
2020-08-24 14:18:24 -0400

    remove my own firebase info

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyDTSemYSFxLH8kekPvHNPp-r5EcTIeMGe4",
-    authDomain: "little-chat-now.firebaseapp.com",
-    databaseURL: "https://little-chat-now.firebaseio.com",
-    projectId: "little-chat-now",
-    storageBucket: "little-chat-now.appspot.com",
-    messagingSenderId: "1000790721434",
-    appId: "1:1000790721434:web:c4a3a8e87c3776b644b023",
-    measurementId: "G-HQP78RSY3G"
+    //input your own firebase config
   });
 
   const db = firebase.firestore();
